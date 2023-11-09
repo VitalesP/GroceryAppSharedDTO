b02fa781b34880df2bbca52a04442b3720ab754b
Vitalii.Peregudov <VitalesP>
2023-11-09 14:37:06 +0600

    fix

--- Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
@@ -11,7 +11,7 @@ public struct LoginResponseDTO: Codable {
     public let error: Bool
     public var reason: String? = nil
     public var token: String? = nil
-    var userId: UUID? = nil
+    public var userId: UUID? = nil
     
     init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
         self.error = error
